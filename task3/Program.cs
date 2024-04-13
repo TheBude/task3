211e1068ba2882b3026857de6cb88ac565d719f8
TheBude <husaenbobmuratov192@gmail.com>
2024-04-13 17:01:51 +0500

    Palindorm tekshiruv

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Matn kiriting: ");
+        string input = Console.ReadLine();
+
+        if (IsPalindrome(input))
+        {
+            Console.WriteLine("Palindrom");
+        }
+        else
+        {
+            Console.WriteLine("Palindrom emas");
+        }
+    }
+
+    static bool IsPalindrome(string text)
+    {
+        text = text.ToLower();
+
+        string reversed = "";
+        for (int i = text.Length - 1; i >= 0; i--)
+        {
+            reversed += text[i];
+        }
+
+        return text == reversed;
+    }
+}
