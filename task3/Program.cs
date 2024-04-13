728d8393cb797b81428523ede683bc2452d8d8e6
TheBude <husaenbobmuratov192@gmail.com>
2024-04-13 18:07:57 +0500

    Tempiratura

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,32 +1,34 @@
-﻿using System;
+﻿//Tempiratura
+Console.WriteLine("Salom Men tempiraturani o'giruvchi dasturman!");
 
-class Program
+Console.Write("Timperaturani Selsiyusda kiriting: ");
+string op = Console.ReadLine();
+int temp = Convert.ToInt32(op);
+
+Console.WriteLine($"{temp} °C ni  Fahrenheit yoki Kelvin ga ko'chiraylikmi");
+
+Console.Write(" Fahrenheit ga bo'lsa '1', Kelvin bo'lsa '2' ni kiriting:  ");
+string num = Console.ReadLine();
+
+switch(op)
 {
-    static void Main()
-    {
-        Console.Write("Matn kiriting: ");
-        string input = Console.ReadLine();
+    case "0":
+        Console.WriteLine($" 0 °C selsiy 32 °F Fahrenheit ga teng\n 0 °C selsiy 273.15 K kelvin ga teng");
+        break;
+    default:
+        Console.WriteLine("afc");
+        break;
+}
 
-        if (IsPalindrome(input))
-        {
-            Console.WriteLine("Palindrom");
-        }
-        else
-        {
-            Console.WriteLine("Palindrom emas");
-        }
-    }
-
-    static bool IsPalindrome(string text)
-    {
-        text = text.ToLower();
-
-        string reversed = "";
-        for (int i = text.Length - 1; i >= 0; i--)
-        {
-            reversed += text[i];
-        }
-
-        return text == reversed;
-    }
+switch(num)
+{
+     case "1":
+            Console.WriteLine($"{temp} °C selsiy {temp * 33.8} °F Fahrenheit ga teng");
+            break;
+     case "2":
+            Console.WriteLine($"{temp} °C selsiy {temp * 274.15} K kelvin ga teng");
+            break;
+     default:
+            Console.WriteLine("Bunaqa Operatsiya Yuq");
+            break;
 }
